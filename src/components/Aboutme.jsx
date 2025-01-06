72707f61815e2486cd81c836fd21cc757c8f4d79
MaxLarry <202080010@psu.palawan.edu.ph>
2025-01-07 00:53:44 +0800

    modifying id text and some elements

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -5,35 +5,55 @@ import SideImage from "../assets/img/eyeyeye1.jpg";
 function Aboutme() {
   return (
     <section
-      className="section home-intro px-9 bg-[--color-dark-dark]"
+      className="section home-intro px-2 lg:px-9 md:p-5 bg-[--color-dark-dark]"
       data-scroll-section
     >
       <div className="container">
         <div className="animation-intro">
-          <div className="title-wrapper absolute flex flex-col text-center">
+          <div className="title-wrapper">
+          <div class="single-title hidden ">
+               <h2 class="xl hidden">Larry<br/> John</h2>
+            </div>
             <div className="title-list ">
               <div className="single-title">
-                <h2 className="xl">No Rules.</h2>
+                <h2 className="xl">There is No<br />Rules.</h2>
               </div>
               <div className="single-title">
-                <h2 className="xl">Create Without<br /> Limits.</h2>
+                <h2 className="xl">
+                  Create Without
+                  <br /> Limits.
+                </h2>
               </div>
               <div className="single-title">
-                <h2 className="xl">Develop Design Inspire</h2>
+                <h2 className="xl">Develop Design<br />Inspire.</h2>
               </div>
             </div>
           </div>
-          <div className="text top-text flex-col flex">
+          <div className="text top-text">
             <h1 className="flex justify-end text-in-right">
               Web Developer/Designer
             </h1>
           </div>
-          <div className="EyeImage w-full justify-center items-center p-5 flex flex-col z-20">
+          <div className="EyeImage w-full justify-center items-center p-[1vw] flex flex-col z-20">
             <img
               src={EyeImage}
               alt="larry-eye.jpg"
               className="object-cover max-h-72 w-5/6 "
             />
+            <div className="absolute z-50 right-0 -bottom-10">
+              <img
+                src={SideImage}
+                alt=""
+                className="object-cover w-[20vw] max-w-60 aspect-square rounded-lg -rotate-3"
+              />
+            </div>
+            <div className="absolute z-50 left-0 -top-10">
+              <img
+                src={SideImage}
+                alt=""
+                className="object-cover w-[20vw] max-w-60 aspect-square rounded-lg rotate-3"
+              />
+            </div>
           </div>
           <div className="text bottom-text flex flex-col justify-start">
             <h1 className="text-in-left">Graphic Designer/Video Editor</h1>
@@ -53,25 +73,10 @@ function Aboutme() {
         </div> */}
 
           {/* image animated* */}
-
-          <div className="absolute z-50 right-0 bottom-10">
-            <img
-              src={SideImage}
-              alt=""
-              className="object-cover lg:w-60 lg:h-60 md:w-52 md:h-52 w-24 h-24 rounded-lg -rotate-3"
-            />
-          </div>
-          <div className="absolute z-50 left-0 top-10">
-            <img
-              src={SideImage}
-              alt=""
-              className="object-cover lg:w-60 lg:h-60 md:w-52 md:h-52 w-24 h-24 rounded-lg rotate-3"
-            />
-          </div>
         </div>
         <div className="text-intro flex flex-col text-center text-[--color-text-light] justify-center items-center">
-          <div className="lg:w-1/2 w-full">
-            passionate and adaptable freelancer delivering high-quality work
+          <div className="lg:w-1/2 w-full px-4 text-sm md:text-lg lg:text-lg">
+            Passionate and adaptable freelancer delivering high-quality work
             that exceeds expectations. Continuously learning and staying ahead
             of industry trends to provide cutting-edge solutions. Committed to
             excellence, I am a reliable asset for any project.
